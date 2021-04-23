23eea3471a98463f475c511fe70eb027189a2874
Albina Shelton <albina.shelton@masterelectronics.com>
2021-04-23 11:53:56 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+// Albina Shelton
+
+const express = require('express')
+const app = express()
+var path = require('path');
+var bodyParser = require('body-parser');
+const port = 3000
+var fs = require('fs');
+const router = express.Router();
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+/**
+ * Error 500
+ */
+ app.use((error, req, res, next) => {
+    res.status(error.status || 500).send({
+        error: {
+            status: error.status || 500,
+            message: error.message || "Error 500"
+        },
+    });
+});
+
+router.get('/', function(req, res) {
+
+    res.sendFile(path.join(__dirname + '/index.html'));
+    
+});
+    //add the router
+    app.use('/', router);
+    app.listen(process.env.port || 3000);
+    
+    console.log('Running at Port 3000');

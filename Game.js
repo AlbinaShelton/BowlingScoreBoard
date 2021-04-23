94ddbd1649d8b7dd8cdc498f181156ed2122fea2
Albina Shelton <albina.shelton@masterelectronics.com>
2021-04-23 14:39:31 -0700

    Created UI

---
+++ Game.js
@@ -0,0 +1,26 @@
+var array = []
+var num1, num2
+var count = 1
+
+function printResult(num) {
+    //if staik
+    if (num === 10) {
+        //wait until next one
+    } else {
+
+        num1 = num
+
+    }
+}
+
+function secondtry(num) {
+    num2 = num
+    var sum = num1 + num2
+    if (sum === 10) {
+        //wait next result
+    } else {
+        console.log(sum)
+    }
+
+}
+printResult(5)
--- main.js
+++ main.js
@@ -9,13 +9,14 @@ var fs = require('fs');
 const router = express.Router();
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
-
+var array = []
+array.length = 10
 
 app.use(express.static(path.join(__dirname, 'public')));
 /**
  * Error 500
  */
- app.use((error, req, res, next) => {
+app.use((error, req, res, next) => {
     res.status(error.status || 500).send({
         error: {
             status: error.status || 500,
@@ -27,10 +28,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 router.get('/', function(req, res) {
 
     res.sendFile(path.join(__dirname + '/index.html'));
-    
+
 });
+
+app.get('/info', (req, res) => {
+        console.log(req.body)
+        var data = req.body
+
+        res.send(data)
+
+    })
     //add the router
-    app.use('/', router);
-    app.listen(process.env.port || 3000);
-    
-    console.log('Running at Port 3000');
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
